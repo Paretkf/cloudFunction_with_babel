ba4ee157a50f406260bdb337fae522c52ad4b890
Paretkf <Paretk7@gmail.com>
2018-06-12 17:15:16 +0700

    do transaction

--- src/functions/index.js
+++ src/functions/index.js
@@ -16,13 +16,11 @@ export let getDiscount = functions.https.onRequest(async (req, res) => {
   let promotion = await db.collection('promoCode').doc(promoCode).get()
   if (promotion.exists) {
     let data = promotion.data()
-    setLog (req.query, state.FIND_CODE, data)
     res.send({
       ...data,
       promoCode: promoCode
     })
   } else {
-    setLog (req.query, state.FIND_CODE, null)
     res.send(null)
   }
 })
@@ -30,34 +28,34 @@ export let getDiscount = functions.https.onRequest(async (req, res) => {
 export let checkOut = functions.https.onRequest(async (req, res) => {
   let {tel, net, promoCode} = req.query
   if (!((tel) && (net)) || !(/^\d+$/.test(net))) {
-    setLog (req.query, state.VALIDATE_DATA, 'Bad Values!!!')
     res.status(400).send('Bad Values!!')
     return
   }
 
   if (promoCode) {
     let now_Date = new Date()
-    const selectedPromotion = await db.runTransaction(transaction => {
+    const promotion = await db.runTransaction(transaction => {
       return transaction.get(db.collection('promoCode').doc(promoCode))
+      .then (async selectedPromotion => {
+        if (selectedPromotion.exists && selectedPromotion.data().status === 'unused' && (selectedPromotion.data().exp_date > now_Date)) {
+          setStatus(selectedPromotion.data().type, promoCode) // set Status to used
+    
+          net = await getNetDiscount (  // set getDiscount Price
+            net,
+            selectedPromotion.data().discount_type,
+            selectedPromotion.data().discount_number
+          )
+        } else {
+          // Log Code can not be used.
+        }
+      })
+    }).then(() => {
+      console.log('Success')
+    }).catch(err => {
+      console.error(err)
     })
-
-    if (selectedPromotion.exists && selectedPromotion.data().status === 'unused' && (selectedPromotion.data().exp_date > now_Date)) {
-      await setStatus(selectedPromotion.data().type, promoCode) // set Status to used
-
-      net = getNetDiscount (  // set getDiscount Price
-        net,
-        selectedPromotion.data().discount_type,
-        selectedPromotion.data().discount_number
-      )
-
-      setLog (req.query, state.USE_CODE, net)
-    } else {
-      setLog (req.query, state.USE_CODE, null)
-    }
   }
-
   const result = await getCode(tel, net)
-  setLog (req.query, state.GEN_CODE, result)
   res.send(result)
 })
 
@@ -70,9 +68,9 @@ function getNetDiscount (net, discountType, discountNumber) { // Test
   return net
 }
 
-async function setStatus (type, promoCode) {
+function setStatus (type, promoCode) {
   if (type === 'onetime') {
-    await db.collection('promoCode').doc(promoCode).update({
+    db.collection('promoCode').doc(promoCode).update({
       status: 'used'
     })
   }
@@ -80,30 +78,43 @@ async function setStatus (type, promoCode) {
 
 async function getCode (tel, net) {
   const result = await db.collection('vip').doc(tel).get()
+  let generatedCode = 'AAAAA'
   if (result.exists && net >= 3000) {
-    const generatedCode = genCode()
-    let createDate = new Date()
-    let expDate = new Date(
-      createDate.getFullYear(),
-      createDate.getMonth()+3,
-      createDate.getDay(),
-      createDate.getHours(),
-      createDate.getMinutes(),
-      createDate.getSeconds()
-    )
-    let newCode = {
-      create_date: createDate,
-      discount_number: 300,
-      discount_type: 'Baht',
-      exp_date: expDate,
-      status: 'unused',
-      type: 'onetime'
-    }
-    await db.collection('promoCode').doc(generatedCode).set(newCode)
+    // await db.collection('promoCode').doc('totalDocumentOfPromotionCode').set(newCode)
+    const promotion = await db.runTransaction(transaction => {
+        return transaction.get(db.collection('promoCode').doc('totalDocumentOfPromotionCode'))
+        .then (async t => {
+          let newDocument = t.data().promoCode
+          while (newDocument.find(code => code === generatedCode) === generatedCode) {
+            generatedCode = 'BBBBB'
+          }
+          newDocument.push(generatedCode)
+          transaction.update(db.collection('promoCode').doc('totalDocumentOfPromotionCode'), { promoCode: newDocument });
+      }).then(async () => {
+        await setNewDocumentPromoCode (generatedCode)
+      }).catch(err => {
+        console.error(err)
+      })
+    })
+    
     return ({ Code : generatedCode, Net : net})
   } else {
     return ({Net: net})
   }
+}
+
+async function setNewDocumentPromoCode (generatedCode) {
+  let createDate = new Date()
+  let expDate = new Date(createDate.getFullYear(), createDate.getMonth()+3, createDate.getDay(), createDate.getHours(), createDate.getMinutes(), createDate.getSeconds())
+  let newCode = {
+    create_date: createDate,
+    discount_number: 300,
+    discount_type: 'Baht',
+    exp_date: expDate,
+    status: 'unused',
+    type: 'onetime'
+  }
+  await db.collection('promoCode').doc(generatedCode).set(newCode)
 }
 
 function genCode() { // check Code
